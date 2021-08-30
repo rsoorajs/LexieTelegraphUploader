fc225868f12ea049bae02ab57c7551c77805c0fb
Vihara Senindu <86075472+viharasenindu@users.noreply.github.com>
2021-08-30 07:45:05 +0530

    test

--- bot.py
+++ bot.py
@@ -38,19 +38,19 @@ async def getimage(client, message):
         text=f"<b>Link :-</b> <code>https://telegra.ph{url_path}</code>",
         disable_web_page_preview=True,
         reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton(
-                        text="Open Link", url=f"https://telegra.ph{url_path}"
-                    ),
-                    InlineKeyboardButton(
+            [[
+                   InlineKeyboardButton(
+                        text="Open Link", url=f"https://telegra.ph{url_path}"),
+            ],[
+                   InlineKeyboardButton(
                         text="Share Link",
                         url=f"https://telegram.me/share/url?url=https://telegra.ph{url_path}",
-                    )
-                ]
-            ]
-        )
-    )
+
+
+             ]]
+                  
+       ),
+ 
     os.remove(img_path)
 
 
