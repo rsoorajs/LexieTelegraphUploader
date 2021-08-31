0cfe60df61de6abfd897187d74226e916fe96c06
Vihara Senindu <86075472+viharasenindu@users.noreply.github.com>
2021-08-31 11:18:20 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -52,14 +52,21 @@ async def getimage(client, message):
     await dwn.edit_text(
         text=f"<b>Link :-</b> <code>https://telegra.ph{url_path}</code>",
         disable_web_page_preview=True,
-        reply_markup=InlineKeyboardMarkup(
+       reply_markup=InlineKeyboardMarkup(
             [
-                   InlineKeyboardButton(
-                        text="Open Link", url=f"https://telegra.ph{url_path}")
+                [
+                    InlineKeyboardButton(
+                        text="Open Link", url=f"https://telegra.ph{url_path}"
+                    ),
+                    InlineKeyboardButton(
+                        text="Share Link",
+                        url=f"https://telegram.me/share/url?url=https://telegra.ph{url_path}",
+                    )
+                ]
             ]
-                  
-       ),
- 
+        )
+    )
     os.remove(img_path)
-tgraph.run()
 
+
+tgraph.run()
