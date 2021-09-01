3eaa28a89ba4dac2e49c9ad1e0eb70cb3eb2ee13
Vihara Senindu <86075472+viharasenindu@users.noreply.github.com>
2021-09-01 14:22:48 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -30,7 +30,7 @@ async def start(c: Client, message: Message):
     if (int("--1001525259563")) is not None:
         try:
             user = await c.get_chat_member(int("-1001525259563")), message.chat.id
-            if user.status == "kicked"
+            if user.status == "kicked":
                 await c.send_message('hi')
 
         return
