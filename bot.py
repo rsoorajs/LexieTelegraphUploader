84966116a00f923aebb44efe135bc4aac97c3bb8
Vihara Senindu <86075472+viharasenindu@users.noreply.github.com>
2021-09-02 00:40:06 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 import os
 import logging
-import from pyrogram Client, filters
+from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, InputTextMessageContent, Message
 from pyrogram.errors import UserNotParticipant, ChatAdminRequired, UsernameNotOccupied 
 from creds import Credentials
