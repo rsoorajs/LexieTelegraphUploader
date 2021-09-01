8b6ced4fb6c669f792d631019704e71453fde2c8
Vihara Senindu <86075472+viharasenindu@users.noreply.github.com>
2021-09-01 13:58:41 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -29,7 +29,7 @@ tgraph = Client(
 async def start(c: Client, message: Message):
     if (int("--1001525259563")) is not None:
         try:
-            user = await c.get_chat_member(int("-1001525259563")), message.chat.id)
+            user = await c.get_chat_member(int("-1001525259563")), message.chat.id')'
             if user.status == "kicked":)
                 await c.send_message('hi')
 
