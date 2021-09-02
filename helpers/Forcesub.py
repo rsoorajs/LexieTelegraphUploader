9e0eeb1aa8887f59b9c2c50dbff7220d3135866f
Vihara Senindu <86075472+viharasenindu@users.noreply.github.com>
2021-09-02 23:27:29 +0530

    Update Forcesub.py

--- helpers/Forcesub.py
+++ helpers/Forcesub.py
@@ -22,10 +22,10 @@ async def ForceSub(bot: Client, event: Message):
         fix_ = await ForceSub(bot, event)
         return fix_
     except Exception as err:
-        print(f"Unable to do Force Subscribe to {info.UPDATES_CHANNEL}\n\nError: {err}\n\nContact Support Group: https://t.me/leosupportx")
+        print(f"Unable to do Force Subscribe to {creds.UPDATES_CHANNEL}\n\nError: {err}\n\nContact Support Group: https://t.me/leosupportx")
         return 200
     try:
-        user = await bot.get_chat_member(chat_id=(int(info.UPDATES_CHANNEL) if info.UPDATES_CHANNEL.startswith("-100") else info.UPDATES_CHANNEL), user_id=event.from_user.id)
+        user = await bot.get_chat_member(chat_id=(int(creds.UPDATES_CHANNEL) if info.UPDATES_CHANNEL.startswith("-100") else creds.UPDATES_CHANNEL), user_id=event.from_user.id)
         if user.status == "kicked":
             await bot.send_message(
                 chat_id=event.from_user.id,
