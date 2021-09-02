b20a4b2b3c2e6f8b75189190580a69cc3b211966
Vihara Senindu <86075472+viharasenindu@users.noreply.github.com>
2021-09-02 19:42:20 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,21 +1,12 @@
-  
 import os
 import logging
 from pyrogram import Client, filters
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, InputTextMessageContent
-from pyrogram.errors import UserNotParticipant, ChatAdminRequired, UsernameNotOccupied 
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from creds import Credentials
 from telegraph import upload_file
 
 logging.basicConfig(level=logging.WARNING)
 
-JOIN_ASAP = "<b>You can't use this command untill you subscribe my channel</b> "
-
-FSUBB = InlineKeyboardMarkup(
-        [[
-        InlineKeyboardButton(text="  Join My Channel 🔔 ", url=f"https://t.me/Lexiebotupdate") 
-        ]]
-    )        
 
 tgraph = Client(
     "Telegraph Uploader bot",
@@ -26,30 +17,22 @@ tgraph = Client(
 
 
 @tgraph.on_message(filters.command("start"))
-
-async def start(client,message):
-    try:
-        await message.client.get_chat_member(int("-1001267157538"), message.from_user.id)
-    except UserNotParticipant:
-        await message.reply_text(
-        text=JOIN_ASAP, disable_web_page_preview=True, reply_markup=FSUBB
-    )
-        return
+async def start(client, message):
     await message.reply_text(
-        text=f"Hello {message.from_user.mention},\nI'm Telegraph Uploader Bot",
+        text=f"Hello {message.from_user.mention},\nI'm a Telegraph Uploader Bot",
         disable_web_page_preview=True
     )
 
 
 @tgraph.on_message(filters.photo)
 async def getimage(client, message):
-    dwn = await message.reply_text("Downloading...", True)
+    dwn = await message.reply_text("Downloading to my server...", True)
     img_path = await message.download()
-    await dwn.edit_text("Uploading...")
+    await dwn.edit_text("Uploading as telegra.ph link...")
     try:
         url_path = upload_file(img_path)[0]
     except Exception as error:
-        await dwn.edit_text(f"Sorry Something Went Wrong😢\n{error}")
+        await dwn.edit_text(f"Oops something went wrong\n{error}")
         return
     await dwn.edit_text(
         text=f"<b>Link :-</b> <code>https://telegra.ph{url_path}</code>",
