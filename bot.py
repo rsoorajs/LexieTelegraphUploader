ff9c4094b8a22a439147a095e34891bc2e35d14b
Vihara Senindu <86075472+viharasenindu@users.noreply.github.com>
2021-09-02 20:37:47 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,12 +1,21 @@
 import os
 import logging
 from pyrogram import Client, filters
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, InputTextMessageContent, Message
+from pyrogram.errors import UserNotParticipant, ChatAdminRequired, UsernameNotOccupied 
+from LexieTelegraphUploader.helpers.forcesub import ForceSub
 from creds import Credentials
 from telegraph import upload_file
 
 logging.basicConfig(level=logging.WARNING)
 
+JOIN_ASAP = "<b>You can't use this command untill you subscribe my channel</b> "
+
+FSUBB = InlineKeyboardMarkup(
+        [[
+        InlineKeyboardButton(text="  Join My Channel 🔔 ", url=f"https://t.me/Lexiebotupdate") 
+        ]]
+    )        
 
 tgraph = Client(
     "Telegraph Uploader bot",
@@ -17,22 +26,25 @@ tgraph = Client(
 
 
 @tgraph.on_message(filters.command("start"))
-async def start(client, message):
+async def start(c: Client, message: Message):
+    FSub = await ForceSub(c, message)
+    if FSub == 400:
+        return
     await message.reply_text(
-        text=f"Hello {message.from_user.mention},\nI'm a Telegraph Uploader Bot",
+        text=f"Hello {message.from_user.mention},\nI'm Telegraph Uploader Bot",
         disable_web_page_preview=True
     )
 
 
 @tgraph.on_message(filters.photo)
-async def getimage(client, message):
-    dwn = await message.reply_text("Downloading to my server...", True)
+async def getimage(Client, message):
+    dwn = await message.reply_text("Downloading...", True)
     img_path = await message.download()
-    await dwn.edit_text("Uploading as telegra.ph link...")
+    await dwn.edit_text("Uploading...")
     try:
         url_path = upload_file(img_path)[0]
     except Exception as error:
-        await dwn.edit_text(f"Oops something went wrong\n{error}")
+        await dwn.edit_text(f"Sorry Something Went Wrong😢\n{error}")
         return
     await dwn.edit_text(
         text=f"<b>Link :-</b> <code>https://telegra.ph{url_path}</code>",
@@ -55,3 +67,4 @@ async def getimage(client, message):
 
 
 tgraph.run()
+
